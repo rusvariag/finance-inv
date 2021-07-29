12a91afdced85fe1cdb0f270e95e9785235cdc8a
Ivan Savin <ivansavin1987@gmail.com>
2021-07-30 02:26:17 +0300

    rewrite device to client side

--- app.py
+++ app.py
@@ -1,48 +1,69 @@
 import os
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, Response, request, jsonify, send_from_directory
 from flask_socketio import SocketIO
 from twilio.rest import Client
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VoiceGrant
+from twilio.twiml.voice_response import Dial, VoiceResponse
 
-# Account SID from twilio.com/console
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-# Auth Token from twilio.com/console
-auth_token  = os.environ['TWILIO_AUTH_TOKEN']
-# twilio client
-client = Client(account_sid, auth_token)
-
+# app  
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+# GET / - server static files
 @app.route('/')
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
 
-# POST /call - send data to twilio
-@app.route('/call', methods=['POST'])
-def make_call():
-    request_data = request.get_json()
-    call = client.calls.create(
-        method='GET',
-        status_callback='https://1729e2371628.ngrok.io/webhook',
-        status_callback_event=['initiated', 'ringing', 'answered', 'completed'],
-        status_callback_method='POST',
-        twiml='<Response><Say>Hello there, its me, Ivan. Will you by this car from me?</Say></Response>',
-        to=request_data['phoneNumber'],
-        from_= os.environ['TWILIO_PHONE']
+@app.route("/token", methods=["GET"])
+def token():
+    # get credentials for environment variables
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    application_sid = os.environ["TWILIO_TWIML_APP_SID"]
+    api_key = os.environ["API_KEY"]
+    api_secret = os.environ["API_SECRET"]
+
+    # Create access token with credentials
+    token = AccessToken(account_sid, api_key, api_secret)
+
+    # Create a Voice grant and add to token
+    voice_grant = VoiceGrant(
+        outgoing_application_sid=application_sid,
     )
-    return jsonify(call.sid)
+    token.add_grant(voice_grant)
+
+    # Return token info as JSON
+    token = token.to_jwt()
+
+    # Return token info as JSON
+    return jsonify(token=token.decode("utf-8"))
+
+
+@app.route("/voice", methods=["POST"])
+def voice():
+    resp = VoiceResponse()
+
+    # Placing an outbound call from the Twilio client
+    dial = Dial(caller_id=os.environ['TWILIO_PHONE'], timeout=10)
+
+    # specify number and webhooks
+    dial.number(request.form["phone"], 
+        status_callback_event='initiated ringing answered completed',
+        status_callback=os.environ['WEBHOOK'],
+        status_callback_method='POST'
+    )
+
+    resp.append(dial)
+
+    return Response(str(resp), mimetype="text/xml")
+
 
 # POST /webhook - recieve data from twilio
 @app.route('/webhook', methods=['POST'])
 def twilio_webhooks():
     socketio.emit('newStatus', request.form.get('CallStatus'))
     return 'Ok'
-
-# GET / - serve html
-@app.route('/', methods=['GET'])
-def home():
-    return 'Hello, world!'
 
 app.run(port=os.environ['PORT'])
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 import socketIOClient from 'socket.io-client';
+import { Device } from '@twilio/voice-sdk';
 
 // import styles
 import './styles.css';
@@ -12,10 +13,8 @@ const NEW_EVENT = 'newStatus';
 
 const App = () => {
   const socketRef = useRef();
-  // initail state for app
-  const initialValues = {
-    phoneNumber: '',
-  };
+  const deviceRed = useRef();
+
   // states
   const [status, setStatus] = useState(undefined);
 
@@ -25,33 +24,43 @@ const App = () => {
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
   const validationSchema = Yup.object({
     phoneNumber: Yup.string()
-      // .matches(phoneRegExp, 'Phone number is not valid')
-      .required('Required'),
+      .matches(phoneRegExp, '* Phone number is not valid')
+      .required('* Required'),
   });
 
+  // start application services
   useEffect(() => {
-    socketRef.current = socketIOClient('');
+    socketRef.current = socketIOClient('/');
     socketRef.current.on(NEW_EVENT, message => {
       setStatus(message);
     });
-  }, []);
 
-  // submit handler
-  const onSubmitHandler = values => {
-    fetch('/call', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(values),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
+    fetch('/token')
+      .then(res => res.json())
+      .then(({ token }) => {
+        deviceRed.current = new Device(token, {
+          warnings: false,
+        });
       })
       .catch(error => {
         console.error('Error:', error);
       });
+  }, []);
+
+  // submit handler
+  const onSubmitHandler = async values => {
+    var params = {
+      // get the phone number to call from the DOM
+      // MAYBE: add country choice
+      phone: `+972${values.phoneNumber}`,
+    };
+    // Twilio.Device.connect() returns a Call object
+    const call = await deviceRed.current.connect({ params });
+
+    // add listeners to the Call
+    call.addListener('accept', () => {
+      console.log('Call in progress ...');
+    });
   };
 
   return (
@@ -59,26 +68,32 @@ const App = () => {
       <h1>Twilio Outbound Call</h1>
       <main className="main">
         <Formik
-          initialValues={initialValues}
+          initialValues={{ phoneNumber: '' }}
           validationSchema={validationSchema}
           onSubmit={onSubmitHandler}
         >
-          <Form className="form">
-            <label className="label" htmlFor="phoneNumber">
-              Enter Phone Number
-            </label>
-            <Field
-              className="input-field"
-              type="text"
-              id="phoneNumber"
-              name="phoneNumber"
-              placeholder="Phone number"
-            />
-            <button type="submit" className="btn">
-              get started
-            </button>
-          </Form>
+          {({ errors, touched }) => (
+            <Form className="form">
+              <label className="label" htmlFor="phoneNumber">
+                Enter Phone Number ( Israel only - without leading zero )
+              </label>
+              <Field
+                className="input-field"
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                placeholder="Phone number"
+              />
+              {errors.phoneNumber && touched.phoneNumber ? (
+                <div className='error'>{errors.phoneNumber}</div>
+              ) : null}
+              <button type="submit" className="btn">
+                get started
+              </button>
+            </Form>
+          )}
         </Formik>
+        <div className="status-label">Call status: </div>
         <div className="status">{JSON.stringify(status)}</div>
       </main>
     </div>
